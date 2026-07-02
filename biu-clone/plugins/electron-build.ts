7ad63e77170a8ae49f14cc9ed30965d6b8afb1c1
An0114 <147520793+An0114@users.noreply.github.com>
2026-07-02 22:14:35 +0800

    add about electrons

---
+++ biu-clone/plugins/electron-build.ts
@@ -0,0 +1,11 @@
+import { logger } from "@rsbuild/core";
+import { build as electronBuild } from "electron-builder"
+
+
+
+export async function buildElectron() {
+    await electronBuild({
+        publish: "onTag",
+        config: 
+    })
+}
---
+++ biu-clone/rsbuild.config.ts
@@ -0,0 +1,54 @@
+import { defineConfig } from "@rsbuild/core";
+import { pluginReact } from "@rsbuild/plugin-react";
+import { pluginSvgr } from "@rsbuild/plugin-svgr";
+
+
+export default defineConfig({
+    output: {
+        distPath: {
+            root: "./dist/web",
+        },
+        //生产环境相对路径，保证通过 file：// 加载时静态资源能够正确引用
+        assetPrefix: "./",
+        cleanDistPath: true,
+    },
+    performance: {
+        removeMomentLocale: true,
+    },
+    html: {
+        template: "./src/index.html"
+    },
+    plugins: [
+        pluginReact(),
+        pluginSvgr({
+            svgrOptions: {
+                exportType: "named",
+                // Enable SVGO to optimize inline SVGs
+                svgo: true,
+                svgoConfig: {
+                    plugins: [
+                        {
+                            name: "preset-default",
+                            params: { overrides: { removeViewBox: false} },
+                        },
+                    ],
+                },
+            },
+        }),
+        
+    ],
+    dev: {
+        writeToDisk: true,
+        lazyCompilation: false,
+        cliShortcuts: false,
+        //开发环境相对路径，保证通过 file:// 加载时静态资源能够正确引用
+        assetPrefix: "./"
+    },
+    server: {
+        port: 5678,
+        strictPort: false,
+        printUrls: false,
+        open: false,
+        compress: false
+    },
+});
