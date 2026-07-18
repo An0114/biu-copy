6ce48904ebb2c13db580ae4af5747f1f0fc74339
An0114 <147520793+An0114@users.noreply.github.com>
2026-07-18 15:38:57 +0800

    rebuild the routes and make some changes in empty,notfound, settings, search

---
+++ biu-clone/src/pages/settings/index.tsx
@@ -0,0 +1,8 @@
+
+const SettingsPage = () => {
+    return (
+        <p>SettingsPage</p>
+    );
+}
+
+export default SettingsPage;
--- biu-clone/src/routes.tsx
+++ biu-clone/src/routes.tsx
@@ -1,11 +1,37 @@
 import type { RouteObject } from "react-router";
 
 import Layout from "./layout";
+import NotFound from "./pages/not-found";
+import Settings from "./pages/settings";
+import Search from "./pages/search";
+import EmptyPage from "./pages/empty";
 
 const routes: RouteObject[] = [
     {
         path: "/",
-        element: <Layout />
+        element: <Layout />,
+        children: [
+            {
+                index: true,
+                element: <NotFound />,
+            },
+            {
+                path: "settings",
+                element: <Settings />,
+            },
+            {
+                path: "search",
+                element: <Search />,
+            },
+            {
+                path: "empty",
+                element: <EmptyPage />,
+            }
+        ]
+    },
+    {
+        path: "*",
+        element: <NotFound />,
     }
 ]
 
